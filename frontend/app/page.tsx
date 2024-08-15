c405a1fe0e3c3c834888c6e70c64f34316a6f2c8
jsndz <jaisondz9360.com>
2024-08-15 10:02:33 +0530

    Deploy fix2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,25 @@ import Foot from "@/components/Home/Foot";
 import Hero from "@/components/Home/Hero";
 import Info from "@/components/Home/Info";
 import NavHome from "@/components/Home/NavHome";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://ephemera.onrender.com");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <NavHome></NavHome>
